d48000d9fd94e42ed6a767929b8a9c1964f3ece1
Carsen Decker <cad631@nyu.edu>
2020-02-01 16:23:26 -0500

    fonts and timer lol

---
+++ Assets/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float YearLimit;
+    public float CurrentYear;
+    public float TimePassSpeed;
+
+    [Space(10)] public TMP_Text YearText;
+
+    private bool gameRunning = true;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(gameRunning)
+            TimePasses();
+    }
+
+    private void TimePasses()
+    {
+        CurrentYear += Time.deltaTime * TimePassSpeed;
+        YearText.text = CurrentYear.ToString("F0") + " AD";
+
+        if (CurrentYear > YearLimit)
+            GameLost();
+    }
+
+    private void GameLost()
+    {
+        Debug.Log("U lose");
+        gameRunning = false;
+    }
+}
---
+++ Assets/Scripts/DecorManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecorManager : MonoBehaviour
+{
+    //List of possible objects to spawn
+    public List<GameObject> GrowableObjects = new List<GameObject>();
+    public List<GameObject> RemovableObjects = new List<GameObject>();
+    
+    //List of objects in the scene
+    public List<GameObject> GrowablesInScene = new List<GameObject>();
+    public List<GameObject> RemovablesInScene = new List<GameObject>();
+
+
+    public void InstantiateRandomObject()
+    {
+//        GameObject newObject = 
+    }
+    
+    
+    public void RemoveRandomObject(float numberToRemove)
+    {
+        for (int i = 0; i < numberToRemove; i++)
+        {
+            GameObject toRemove = RemovablesInScene[Random.Range(0, RemovablesInScene.Count)];
+            RemovablesInScene.Remove(toRemove);
+            
+            Destroy(toRemove);
+        }
+    }
+
+    public void GrowRandomObject(float numberToGrow)
+    {
+        for (int i = 0; i < numberToGrow; i++)
+        {
+            GameObject toGrow = GrowablesInScene[Random.Range(0, RemovablesInScene.Count)];
+            toGrow.GetComponent<Growable>().Grow();
+
+            RemovablesInScene.Remove(toGrow);
+
+            
+            Destroy(toGrow);
+            
+        }
+    }
+}
