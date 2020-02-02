da398509f9e0ea265e8b157361fd08152d89471c
Samantha Chuang <samantha.s.chuang@gmail.com>
2020-02-01 20:20:52 -0500

    Updated Sprites

--- Assets/Scripts/CarbonDioxideMovement.cs
+++ Assets/Scripts/CarbonDioxideMovement.cs
@@ -2,7 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarbonDioxideMovement : MonoBehaviour
-{
+public class CarbonDioxideMovement : MonoBehaviour {
+    private Vector2 movement;
+    private float speed;
+    public Rigidbody2D rb;
+    private int pushForce = 10;
 
+    // Start is called before the first frame update
+    void Start() {
+        speed = Random.Range(-10f, 10f);
+        rb = GetComponent<Rigidbody2D>();
+        movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Wall") {
+            Vector2 flingForce = Vector2.Reflect(movement, -other.transform.up);
+            rb.AddForce(flingForce);
+        }
+    }
 }
